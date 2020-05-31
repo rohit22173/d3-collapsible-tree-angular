196be0c86eacdeda09e7d6b86055cdb5064e2a6a
Rohit Kawade <Rohit.Kawade@ey.com>
2020-05-31 20:21:35 -0300

    Added D3 collapsible tree example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,230 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import * as d3 from 'd3';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements AfterViewInit {
+  private _jsonURL = 'assets/data/tree-data.json';
+  treeData;
+  margin = { top: 20, right: 90, bottom: 30, left: 90 };
+  width = 960 - this.margin.left - this.margin.right;
+  height = 500 - this.margin.top - this.margin.bottom;
+
+  // append the svg object to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  treeDiv = document.getElementById('tree');
+  svg;
+
+  i = 0;
+  duration = 750;
+  root;
+
+  // declares a tree layout and assigns the size
+  treemap = d3.tree().size([this.height, this.width]);
+
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit(): void {
+    this.getJSON().subscribe((data) => {
+      this.treeData = data;
+      this.renderTree();
+    });
+  }
+
+  renderTree() {
+    this.svg = d3
+      .select('#collapsible-tree')
+      .append('svg')
+      .attr('width', this.width + this.margin.right + this.margin.left)
+      .attr('height', this.height + this.margin.top + this.margin.bottom)
+      .append('g')
+      .attr(
+        'transform',
+        'translate(' + this.margin.left + ',' + this.margin.top + ')'
+      );
+    // Assigns parent, children, height, depth
+    this.root = d3.hierarchy(this.treeData, function (d) {
+      return d.children;
+    });
+    this.root.x0 = this.height / 2;
+    this.root.y0 = 0;
+    // Collapse after the second level
+    this.root.children.forEach(this.collapse);
+    this.update(this.root);
+  }
+
+  getJSON(): Observable<any> {
+    return this.http.get(this._jsonURL);
+  }
+
+  // Collapse the node and all it's children
+  collapse = (d) => {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(this.collapse);
+      d.children = null;
+    }
+  };
+
+  update(source) {
+    // Assigns the x and y position for the nodes
+    var treeData = this.treemap(this.root);
+
+    // Compute the new tree layout.
+    var nodes = treeData.descendants(),
+      links = treeData.descendants().slice(1);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function (d) {
+      d.y = d.depth * 180;
+    });
+
+    // ****************** Nodes section ***************************
+
+    // Update the nodes...
+    let node = this.svg.selectAll('g.node').data(nodes, function (d) {
+      return d.id || (d.id = ++this.i);
+    });
+
+    // Enter any new modes at the parent's previous position.
+    let nodeEnter = node
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .attr('transform', function (d) {
+        return 'translate(' + source.y0 + ',' + source.x0 + ')';
+      })
+      .on('click', this.click);
+
+    // Add Circle for the nodes
+    nodeEnter
+      .append('circle')
+      .attr('class', 'node')
+      .attr('r', 1e-6)
+      .style('fill', function (d) {
+        return d._children ? 'lightsteelblue' : '#fff';
+      });
+
+    // Add labels for the nodes
+    nodeEnter
+      .append('text')
+      .attr('dy', '.35em')
+      .attr('x', function (d) {
+        return d.children || d._children ? -13 : 13;
+      })
+      .attr('text-anchor', function (d) {
+        return d.children || d._children ? 'end' : 'start';
+      })
+      .text(function (d) {
+        return d.data.name;
+      });
+
+    // UPDATE
+    var nodeUpdate = nodeEnter.merge(node);
+
+    // Transition to the proper position for the node
+    nodeUpdate
+      .transition()
+      .duration(this.duration)
+      .attr('transform', function (d) {
+        return 'translate(' + d.y + ',' + d.x + ')';
+      });
+
+    // Update the node attributes and style
+    nodeUpdate
+      .select('circle.node')
+      .attr('r', 10)
+      .style('fill', function (d) {
+        return d._children ? 'lightsteelblue' : '#fff';
+      })
+      .attr('cursor', 'pointer');
+
+    // Remove any exiting nodes
+    var nodeExit = node
+      .exit()
+      .transition()
+      .duration(this.duration)
+      .attr('transform', function (d) {
+        return 'translate(' + source.y + ',' + source.x + ')';
+      })
+      .remove();
+
+    // On exit reduce the node circles size to 0
+    nodeExit.select('circle').attr('r', 1e-6);
+
+    // On exit reduce the opacity of text labels
+    nodeExit.select('text').style('fill-opacity', 1e-6);
+
+    // ****************** links section ***************************
+
+    // Update the links...
+    let link = this.svg.selectAll('path.link').data(links, function (d) {
+      return d.id;
+    });
+
+    // Enter any new links at the parent's previous position.
+    let linkEnter = link
+      .enter()
+      .insert('path', 'g')
+      .attr('class', 'link')
+      .attr('d', function (d) {
+        var o = { x: source.x0, y: source.y0 };
+        return diagonal(o, o);
+      });
+
+    // UPDATE
+    var linkUpdate = linkEnter.merge(link);
+
+    // Transition back to the parent element position
+    linkUpdate
+      .transition()
+      .duration(this.duration)
+      .attr('d', function (d) {
+        return diagonal(d, d.parent);
+      });
+
+    // Remove any exiting links
+    let linkExit = link
+      .exit()
+      .transition()
+      .duration(this.duration)
+      .attr('d', function (d) {
+        var o = { x: source.x, y: source.y };
+        return diagonal(o, o);
+      })
+      .remove();
+
+    // Store the old positions for transition.
+    nodes.forEach(function (d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+
+    function diagonal(s, d) {
+      let path = `M ${s.y} ${s.x}
+              C ${(s.y + d.y) / 2} ${s.x},
+              ${(s.y + d.y) / 2} ${d.x},
+              ${d.y} ${d.x}`;
+      return path;
+    }
+  }
+
+  // Toggle children on click.
+  click = (d) => {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+    this.update(d);
+  };
 }
